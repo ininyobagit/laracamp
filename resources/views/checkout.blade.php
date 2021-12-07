44c631e5653d03a86729cf89e596dcc597dc0284
ininyobagit <xboxx8044@gmail.com>
2021-12-08 06:35:31 +0700

    Slicing Success Checkout Page

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -27,7 +27,10 @@
                     </div>
                     <div class="col-lg-1 col-12"></div>
                     <div class="col-lg-6 col-12">
-                        <form action="#" class="basic-form">
+                        <form
+                            action="{{ route('success-checkout') }}"
+                            class="basic-form"
+                        >
                             <div class="mb-4">
                                 <label
                                     for="exampleInputEmail1"
